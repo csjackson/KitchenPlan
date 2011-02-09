9ab3d2533c856c3cfe9a7c63e53ce60e2e697653
Christopher Jackson <csjackson@gmail.com>
2011-02-08 21:25:22 -0600

    Initial MVC Project & Tests

---
+++ src/KitchenPlan.Test/HomeControllerTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KitchenPlan.Controllers;
+using System.Web.Mvc;
+
+namespace KitchenPlan.Test
+{
+    [TestClass]
+    public class HomeControllerTests
+    {
+        [TestMethod]
+        public void Index_SHould_Return_A_View_With_A_Title()
+        {
+            // Arrange
+            var cntrl = new HomeController();
+
+            // Act
+            var result = cntrl.Index();
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            var view = result as ViewResult;  // soft cast - will return null if it can't
+           // var view = (ViewResult) result; hard cast - will throw exception if it can't 
+            Assert.AreEqual("Home", view.ViewBag.Title);
+        }
+        [TestMethod]
+        public void About_Should_Return_A_View_With_A_Title()
+        {
+            // Arrange
+            var cntrl = new HomeController();
+
+            // Act
+            var result = cntrl.About();
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            var view = result as ViewResult;  // soft cast - will return null if it can't
+            // var view = (ViewResult) result; hard cast - will throw exception if it can't 
+            Assert.AreEqual("Home", view.ViewBag.Title);
+        }
+
+    }
+}
---
+++ src/KitchenPlan/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KitchenPlan.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home";
+            return View();
+        }
+        public ActionResult About()
+        {
+           ViewBag.Title = "Home";
+            return View();
+        }
+        //protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        //{
+        //    ViewBag.Title = "Home";
+        //    base.OnActionExecuting(filterContext);
+        //}
+    }
+}
