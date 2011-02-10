1488ee2e8ebf5325bf96d07ba0e2e65661120e7d
Christopher Jackson <csjackson@gmail.com>
2011-02-10 15:32:37 -0600

    Added Skeletal Bio & Pic Pages

---
+++ src/KitchenPlan.Test/MeControllerTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KitchenPlan.Controllers;
+using System.Web.Mvc;
+
+namespace KitchenPlan.Test
+{
+    [TestClass]
+    public class MeControllerTests
+    {
+        [TestMethod]
+        public void Bio_Should_Return_A_View_With_A_Title()
+        {
+            // Arrange
+            var cntrl = new MeController();
+
+            // Act
+            var result = cntrl.Bio();
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            var view = result as ViewResult;  // soft cast - will return null if it can't
+            // var view = (ViewResult) result; hard cast - will throw exception if it can't 
+            Assert.AreEqual("Bio", view.ViewBag.Title);
+        }
+        [TestMethod]
+        public void HeadShot_Should_Return_A_View_With_A_Title()
+        {
+            // Arrange
+            var cntrl = new MeController();
+
+            // Act
+            var result = cntrl.HeadShot();
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            var view = result as ViewResult;  // soft cast - will return null if it can't
+            // var view = (ViewResult) result; hard cast - will throw exception if it can't 
+            Assert.AreEqual("HeadShot", view.ViewBag.Title);
+        }
+    }
+}
---
+++ src/KitchenPlan/Controllers/MeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KitchenPlan.Controllers
+{
+    public class MeController : Controller
+    {
+        //
+        // GET: /Me/
+
+        public ActionResult Bio()
+        {
+            ViewBag.Title = "Bio";
+            return View();
+        }
+        public ActionResult HeadShot()
+        {
+            ViewBag.Title = "HeadShot";
+            return View();
+        }
+    }
+}
